369f80c42309d0fea0f0f7d8d41550cf006ab936
mobin <mobinmohammadi4321@gmail.com>
2025-11-17 14:43:44 +0330

    updated

--- src/assets/Components/RestaurantCategoriesPc/RestaurantCategoriesPc.tsx
+++ src/assets/Components/RestaurantCategoriesPc/RestaurantCategoriesPc.tsx
@@ -4,52 +4,52 @@ import CategoriesTitle from "../CategoriesTitle/CategoriesTitle";
 import "./StyleSlider/StyleSlider.css";
 
 export default function RestaurantCategoriesPc() {
-  const DataRestaurantCategoriesPc = [
-    {
-      title: "ایرانی",
-      img: "https://cdn.snappfood.ir/uploads/images/tags/website_image_irani_1.jpg",
-    },
-    {
-      title: "فست فود",
-      img: "https://cdn.snappfood.ir/uploads/images/tags/website_image_fastfood_1.jpg",
-    },
-    {
-      title: "کباب",
-      img: "https://cdn.snappfood.ir/uploads/images/tags/website_image_kebab_1.jpg",
-    },
-    {
-      title: "پیتزا",
-      img: "https://cdn.snappfood.ir/uploads/images/tags/website_image_pizza_1.jpg",
-    },
-    {
-      title: "برگر",
-      img: "https://cdn.snappfood.ir/uploads/images/tags/website_image_burger_1.jpg",
-    },
-    {
-      title: "ساندویچ",
-      img: "https://cdn.snappfood.ir/uploads/images/tags/website_image_sandwich_1.jpg",
-    },
-    {
-      title: "سوخاری",
-      img: "https://cdn.snappfood.ir/uploads/images/tags/website_image_sokhari_1.jpg",
-    },
-    {
-      title: "پاستا",
-      img: "https://cdn.snappfood.ir/uploads/images/tags/website_image_italy_1.jpg",
-    },
-    {
-      title: "دریایی",
-      img: "https://cdn.snappfood.ir/uploads/images/tags/website_image_seafood_1.jpg",
-    },
-    {
-      title: "بین الملل",
-      img: "https://cdn.snappfood.ir/uploads/images/tags/website_image_asian_1.jpg",
-    },
-    {
-      title: "گیلانی",
-      img: "https://cdn.snappfood.ir/uploads/images/tags/website_image_gilani_1.jpg",
-    },
-  ];
+  // const DataRestaurantCategoriesPc = [
+  //   {
+  //     title: "ایرانی",
+  //     img: "https://cdn.snappfood.ir/uploads/images/tags/website_image_irani_1.jpg",
+  //   },
+  //   {
+  //     title: "فست فود",
+  //     img: "https://cdn.snappfood.ir/uploads/images/tags/website_image_fastfood_1.jpg",
+  //   },
+  //   {
+  //     title: "کباب",
+  //     img: "https://cdn.snappfood.ir/uploads/images/tags/website_image_kebab_1.jpg",
+  //   },
+  //   {
+  //     title: "پیتزا",
+  //     img: "https://cdn.snappfood.ir/uploads/images/tags/website_image_pizza_1.jpg",
+  //   },
+  //   {
+  //     title: "برگر",
+  //     img: "https://cdn.snappfood.ir/uploads/images/tags/website_image_burger_1.jpg",
+  //   },
+  //   {
+  //     title: "ساندویچ",
+  //     img: "https://cdn.snappfood.ir/uploads/images/tags/website_image_sandwich_1.jpg",
+  //   },
+  //   {
+  //     title: "سوخاری",
+  //     img: "https://cdn.snappfood.ir/uploads/images/tags/website_image_sokhari_1.jpg",
+  //   },
+  //   {
+  //     title: "پاستا",
+  //     img: "https://cdn.snappfood.ir/uploads/images/tags/website_image_italy_1.jpg",
+  //   },
+  //   {
+  //     title: "دریایی",
+  //     img: "https://cdn.snappfood.ir/uploads/images/tags/website_image_seafood_1.jpg",
+  //   },
+  //   {
+  //     title: "بین الملل",
+  //     img: "https://cdn.snappfood.ir/uploads/images/tags/website_image_asian_1.jpg",
+  //   },
+  //   {
+  //     title: "گیلانی",
+  //     img: "https://cdn.snappfood.ir/uploads/images/tags/website_image_gilani_1.jpg",
+  //   },
+  // ];
 
   return (
     <div className="pb-50 flex flex-col gap-5 container-custom ">
